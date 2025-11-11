f5ae595f33d0be23af1edabe7eb6e13204701cf9
sasunih <195261773+sasunih@users.noreply.github.com>
2025-11-11 14:24:49 +1100

    Enhance Seurat export/import functions
    Refactor Seurat export and import functions to handle optional obsm.csv file and improve spatial data handling.

--- seurat_functions.R
+++ seurat_functions.R
@@ -2,10 +2,8 @@ library("Seurat")
 library("readr")
 library("dplyr")
 library("stringr")
-library("SpatialExperiment")
 
 ####################################### Export out of Seurat
-# Are spatial coords in centroids, or in dimension reduction = centroids_exist
 Seurat_to_csv <- function(seurat_obj, export_dir){
   
   if (!file.exists(export_dir)){
@@ -26,17 +24,28 @@ Seurat_to_csv <- function(seurat_obj, export_dir){
   write.table(var_mat, paste(export_dir,"/var.csv", sep = ""), sep = ",")
   
   # Reductions and Spatial Coordinates
+  if ("pca" %in% names(seurat_obj)){
   X_pca <- seurat_obj@reductions$pca@cell.embeddings
-  colnames(X_pca) <- sapply(1:ncol(X_pca), function(x){paste("X_pca", x, sep = "")})
+  colnames(X_pca) <- sapply(1:ncol(X_pca), function(x){paste("X_pca", x, sep = "")})}
+  if ("umap" %in% names(seurat_obj)){
   X_umap <- seurat_obj@reductions$umap@cell.embeddings
-  colnames(X_umap) <- sapply(1:ncol(X_umap), function(x){paste("X_umap", x, sep = "")})
+  colnames(X_umap) <- sapply(1:ncol(X_umap), function(x){paste("X_umap", x, sep = "")})}
   
+  if (!is.null(Images(seurat_obj))){
   spatial_list<- lapply(Images(seurat_obj), function(x){seurat_obj@images[[x]]$centroids@coords})
   spatial <- do.call("rbind", spatial_list)
-  colnames(spatial) <- sapply(1:ncol(spatial), function(x){paste("spatial",x,sep = "")})
+  colnames(spatial) <- sapply(1:ncol(spatial), function(x){paste("spatial",x,sep = "")})}
   
-  obsm_mat <- cbind(X_pca,X_umap,spatial)
-  write.table(obsm_mat,paste(export_dir, "/obsm.csv", sep = ""), row.names = FALSE, sep = ",")}
+  vars <- c("X_pca", "X_umap", "spatial")
+  # Get only the ones that exist
+  env <- environment()
+  existing_vars <- vars[sapply(vars, exists, envir = env)]
+  # Combine them if any exist
+  if (length(existing_vars) > 0) {
+    obsm_mat <- do.call(cbind, mget(existing_vars))
+    write.table(obsm_mat,paste(export_dir, "/obsm.csv", sep = ""), row.names = FALSE, sep = ",")
+  }
+}
 
 ########################################################
 # Import into Seurat
@@ -44,38 +53,52 @@ csv_to_Seurat <- function(import_dir, assay_name){
   counts_mat <- read.table(paste(import_dir, "/X.csv", sep = ""), header = FALSE, sep = ",", fill = TRUE) %>% as.matrix() %>% t()
   obs_mat <- read.table(paste(import_dir,"/obs.csv", sep = ""), header = TRUE, sep = ",", row.names = 1, fill = TRUE)
   var_mat <- read.table(paste(import_dir,"/var.csv", sep = ""), header = TRUE, sep = ",", row.names = 1, fill = TRUE)
-  obsm_mat <- read.table(paste(import_dir, "/obsm.csv", sep = ""), header = TRUE, sep = ",", fill = TRUE)
+  if (file.exists(paste(import_dir,"/obsm.csv", sep = ""))){
+    content <- readLines(paste(import_dir, '/obsm.csv', sep = ""), n = 1, warn = FALSE)
+    if (content != ""){
+      obsm_mat <- read.table(paste(import_dir, "/obsm.csv", sep = ""), header = TRUE, sep = ",", fill = TRUE)}
+  }
   
   rownames(counts_mat) <- rownames(var_mat)
   colnames(counts_mat) <- rownames(obs_mat)
   
+  seurat_import <- CreateSeuratObject(counts = counts_mat, assay = assay_name)
+  seurat_import <- AddMetaData(seurat_import, obs_mat)
+  
+  if (exists("obsm_mat")){
+  if (any(startsWith(names(obsm_mat), "X_pca"))){
   X_pca <- select(obsm_mat, starts_with('X_pca'))
   colnames(X_pca) <- sapply(1:ncol(X_pca), function(x){paste("pca", x, sep = "_")})
   rownames(X_pca) <- rownames(obs_mat)
+  seurat_import[["pca"]] <- CreateDimReducObject(embeddings = as.matrix(X_pca), key = "pca_", assay = DefaultAssay(seurat_import))
+  }
   
+  if (any(startsWith(names(obsm_mat), "X_umap"))){
   X_umap <- select(obsm_mat,starts_with('X_umap'))
   colnames(X_umap) <- sapply(1:ncol(X_umap), function(x){paste("umap", x, sep = "_")})
   rownames(X_umap) <- rownames(obs_mat)
+  seurat_import[["umap"]] <- CreateDimReducObject(embeddings = as.matrix(X_umap), key = "umap_", assay = DefaultAssay(seurat_import))
+  }
   
+  if (any(startsWith(names(obsm_mat), "spatial"))){
   spatial <- select(obsm_mat, starts_with("spatial"))
   colnames(spatial) <- sapply(1:ncol(spatial), function(x){paste("spatial", x, sep = "_")})
   rownames(spatial) <- rownames(obs_mat)
-  
-  seurat_import <- CreateSeuratObject(counts = counts_mat, assay = assay_name)
-  seurat_import <- AddMetaData(seurat_import, obs_mat)
-  seurat_import[["pca"]] <- CreateDimReducObject(embeddings = as.matrix(X_pca), key = "pca_", assay = DefaultAssay(seurat_import))
-  seurat_import[["umap"]] <- CreateDimReducObject(embeddings = as.matrix(X_umap), key = "umap_", assay = DefaultAssay(seurat_import))
   seurat_import[["spatial"]] <- CreateFOV(CreateCentroids(spatial[,c(1,2)]), assay = assay_name)
+  }
+  }
   
   return(seurat_import)
 }
 
 Seurat_to_SPE <- function(seurat_obj){
+  library("SpatialExperiment")
   spE <- SpatialExperiment(assay = GetAssayData(seurat_obj), colData = seurat_obj[[]], spatialCoords = seurat_obj@images$image$centroids@coords)
   return(spE)
 }
 
 Seurat_to_SPIATSPE <- function(seurat_obj){
+  library('SPIAT')
   spE <- format_image_to_spe(format = "general", 
                              intensity_matrix = as.matrix(GetAssayData(seurat_obj)),
                              phenotypes = as.character(Idents(seurat_obj)), 
