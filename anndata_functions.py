488f09aa79ef814e5eff835b126622ac70048a17
sasunih <195261773+sasunih@users.noreply.github.com>
2025-11-11 14:22:29 +1100

    Improve handling of obsm.csv in csv_to_Anndata
    Added conditional handling for obsm.csv file to avoid errors when it doesn't exist. Updated logic to populate adata_import.obsm with PCA, UMAP, and spatial data only if obsm_df is defined.

--- anndata_functions.py
+++ anndata_functions.py
@@ -14,7 +14,12 @@ def csv_to_Anndata(import_dir): # import dir = location of your .csv files
     X = pd.read_csv(import_dir + "/X.csv", header = None)
     obs = pd.read_csv(import_dir + "/obs.csv", header = 0, index_col = 0)
     var = pd.read_csv(import_dir + "/var.csv", header = 0, index_col = 0)
-    obsm_df = pd.read_csv(import_dir + "/obsm.csv", header = 0)
+    if os.path.exists(import_dir + "/obsm.csv"):
+        with open(import_dir + "/obsm.csv", 'r') as f:
+            first_line = f.readline().strip()
+        if first_line != '':
+            obsm_df = pd.read_csv(import_dir + "/obsm.csv", header = 0)
+        
     X.columns = var.index
 
     adata_import = AnnData(X = X, obs = obs, var = var)
@@ -22,22 +27,27 @@ def csv_to_Anndata(import_dir): # import dir = location of your .csv files
     X_umap = list()
     spatial = list()
 
-    for colname in obsm_df.columns:
-        if 'X_pca' in colname:
-            X_pca.append(obsm_df[colname].to_list())
-        elif 'X_umap' in colname:
-            X_umap.append(obsm_df[colname].to_list())
-        elif 'spatial' in colname:
-            spatial.append(obsm_df[colname].to_list())
-
-    if len(X_pca) != 0:
-        X_pca = np.swapaxes(np.array(X_pca, dtype = np.float32),0,1)
-        adata_import.obsm['X_pca'] = X_pca
-    if len(X_umap) != 0:
-        X_umap = np.swapaxes(np.array(X_umap, dtype = np.float32),0,1)
-        adata_import.obsm['X_umap'] = X_umap
-    if len(spatial) != 0:
-        spatial = np.swapaxes(np.array(spatial), 0, 1)
-        adata_import.obsm['spatial'] = spatial
+    try:
+        obsm_df
+    except NameError:
+        pass
+    else:
+        for colname in obsm_df.columns:
+            if 'X_pca' in colname:
+                X_pca.append(obsm_df[colname].to_list())
+            elif 'X_umap' in colname:
+                X_umap.append(obsm_df[colname].to_list())
+            elif 'spatial' in colname:
+                spatial.append(obsm_df[colname].to_list())
+    
+        if len(X_pca) != 0:
+            X_pca = np.swapaxes(np.array(X_pca, dtype = np.float32),0,1)
+            adata_import.obsm['X_pca'] = X_pca
+        if len(X_umap) != 0:
+            X_umap = np.swapaxes(np.array(X_umap, dtype = np.float32),0,1)
+            adata_import.obsm['X_umap'] = X_umap
+        if len(spatial) != 0:
+            spatial = np.swapaxes(np.array(spatial), 0, 1)
+            adata_import.obsm['spatial'] = spatial
         
     return adata_import # returns adata object
